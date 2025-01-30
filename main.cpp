cf09a3c431cc7707d061924c95cd261d36bcbe41
mm-tvkl <mm.tvkkl@gmail.com>
2025-01-30 13:13:01 +0330

    initialization code

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (void){
+    
+    // int a;       // Defualt initialization
+    // int b = 5;   // Copy initialization
+    // int c ( 5 ); // Direct initialization
+    // int d { 5 }; // Uniform initialization
+
+    return 0;
+}
