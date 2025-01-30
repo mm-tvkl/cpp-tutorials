bcfa34652cf6d168d37bd3d9ca9c31f2272e522d
mm-tvkl <mm.tvkkl@gmail.com>
2025-01-30 13:14:19 +0330

    Functions code

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 
 using namespace std;
 
+void printHi(string name){
+    cout << "Hello " << name;
+}
+
 int main (void){
     
     // int a;       // Defualt initialization
     // int b = 5;   // Copy initialization
     // int c ( 5 ); // Direct initialization
     // int d { 5 }; // Uniform initialization
+
+    printHi("Mahdi");
 
     return 0;
 }
