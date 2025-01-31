e1491cc185af0b27c2ac2e4547889f5d7d278781
mm-tvkl <mm.tvkkl@gmail.com>
2025-01-31 15:16:16 +0330

    Integer and floating point number added to file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
-#include "add.h"
+#include <cstdint>
+#include <iomanip>
+// #include "add.h"
 
 using namespace std;
 
@@ -20,9 +22,35 @@ int main (void){
     // Definition => Writing main structure
     // Declaration => Hey Compilerrrr !!!
 
-    cout << add(10, 11) << '\n';
-    cout << multipliction(10, 11) << '\n';
-    cout << substract(10, 11) << '\n';
+    // cout << add(10, 11) << '\n';
+    // cout << multipliction(10, 11) << '\n';
+    // cout << substract(10, 11) << '\n';
 
+    //! Integer
+    //* short a = 10;
+    //* int b = 20;
+    //* long c = 10;
+    //* long long d = 20;
+
+    //* unsigned short a = 10;
+    //* unsigned int b = 20;
+    //* unsigned long c = 10;
+    //* unsigned long long d = 20;
+
+    //! Floating point
+    // float
+    // double
+    // long double
+    // cout << setprecision(10);
+    // cout << 123456.7891011 << '\n';
+    // cout << 123456.7891011 << '\n';
+    // cout << 123456.7891011 << '\n';
+
+    // float a{0.2};
+    // float b{0.1};
+    // float c{0.3};
+
+    // cout << (a + b == c);
+    
     return 0;
 }
