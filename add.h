b6a5c1c8806dbc40bd1df60426032728f6a83aef
mm-tvkl <mm.tvkkl@gmail.com>
2025-01-31 11:29:45 +0330

    With another header guard

--- add.h
+++ add.h
@@ -1,5 +1,9 @@
-#pragma once
+#ifndef ADD_H
+#define ADD_H
+
 int add (int num1, int num2);
 int substract(int num1, int num2);
 int multipliction(int num1, int num2);
 int division(int num1, int num2);
+
+#endif
